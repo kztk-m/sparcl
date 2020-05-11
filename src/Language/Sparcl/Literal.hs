f44b542addc1b45b951416f84d30781f2f6ac363
Kazutaka Matsuda <kztk@ecei.tohoku.ac.jp>
2020-05-11 18:16:09 +0900

    "Stylish Haskell"ed.

--- src/Language/Sparcl/Literal.hs
+++ src/Language/Sparcl/Literal.hs
@@ -1,25 +1,25 @@
 module Language.Sparcl.Literal where
 
-import Language.Sparcl.Pretty 
+import           Language.Sparcl.Pretty
 import qualified Text.PrettyPrint.ANSI.Leijen as D
 
-import Control.DeepSeq 
+import           Control.DeepSeq
 
 data Literal
   = LitInt      Int
   | LitDouble   Double
   | LitChar     Char
-  | LitRational Rational 
+  | LitRational Rational
   deriving Show
 
 instance NFData Literal where
-  rnf (LitInt i) = rnf i
-  rnf (LitDouble d) = rnf d
-  rnf (LitChar c) = rnf c
-  rnf (LitRational q) = rnf q 
+  rnf (LitInt i)      = rnf i
+  rnf (LitDouble d)   = rnf d
+  rnf (LitChar c)     = rnf c
+  rnf (LitRational q) = rnf q
 
 instance Pretty Literal where
   ppr (LitInt i)      = D.int i
   ppr (LitDouble d)   = D.double d
-  ppr (LitChar c)     = D.text (show c) 
-  ppr (LitRational q) = D.rational q 
+  ppr (LitChar c)     = D.text (show c)
+  ppr (LitRational q) = D.rational q
