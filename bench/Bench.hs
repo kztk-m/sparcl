f92ecf97d7836593191476c9ca0601c6567ac9a8
Kazutaka Matsuda <kztk@tohoku.ac.jp>
2026-07-31 18:48:47 +0900

    Make Bench.hs compatible with the current version

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,41 +1,39 @@
-import           Control.Exception               (evaluate)
-import           Control.Monad.Catch             (MonadCatch, MonadThrow)
-import           Control.Monad.IO.Class
-import           Control.Monad.Reader
+import Control.Exception (evaluate)
+import Control.Monad.IO.Class
+import Control.Monad.Reader
 
-import           Text.Printf
+import Text.Printf
 
-import           Language.Sparcl.Class
-import           Language.Sparcl.DebugPrint
-import           Language.Sparcl.Exception
-import           Language.Sparcl.Module          (ModuleInfo (..),
-                                                  baseModuleInfo)
-import           Language.Sparcl.Pretty          hiding ((<$>))
-import           Language.Sparcl.Renaming
-import           Language.Sparcl.Surface.Parsing
-import           Language.Sparcl.Surface.Syntax  (Module (..))
-import           Language.Sparcl.Typing.TCMonad
-import           Language.Sparcl.Typing.Typing
+import Language.Sparcl.Exception
+import Language.Sparcl.Module (
+  ModuleContext (..),
+  ModuleInfo (..),
+  baseModuleInfo,
+ )
+import Language.Sparcl.Pretty hiding ((<$>))
+import Language.Sparcl.Renaming
+import Language.Sparcl.Surface.Parsing
+import Language.Sparcl.Surface.Syntax (Module (..))
+import Language.Sparcl.Typing.TCMonad
+import Language.Sparcl.Typing.Typing
 
+import Control.Monad (replicateM)
+import qualified System.Clock as Clock
 
-import qualified System.Clock                    as Clock
-
-
-{-# SPECIALIZE bench :: String -> Int -> M a -> M a #-}
-bench :: forall m a. MonadIO m => String -> Int -> m a -> m a
+{-# SPECIALIZE bench :: String -> Int -> TC a -> TC a #-}
+bench :: forall m a. (MonadIO m) => String -> Int -> m a -> m a
 bench str trials m = do
   !s <- liftIO $ Clock.getTime Clock.Monotonic
   !res <- replicateM trials (withTime m)
   !e <- liftIO $ Clock.getTime Clock.Monotonic
 
   let diff :: Double = fromIntegral (Clock.toNanoSecs (Clock.diffTimeSpec e s)) / (10 ** 6) -- in seconcs
-
   liftIO $ putStrLn $ printf "Elapsed (%s): %2.4f ms" str (diff / fromIntegral trials)
 
   let ts = map snd res
   let tt = sum ts
   let mean :: Double = fromInteger tt / fromIntegral trials / (10 ** 6) -- in seconds
-  let var  :: Double = fromInteger (sum [ (a * fromIntegral trials - tt)^(2 :: Integer) | a <- ts ]) / (fromIntegral trials ** 3) / (10 ** 12)
+  let var :: Double = fromInteger (sum [(a * fromIntegral trials - tt) ^ (2 :: Integer) | a <- ts]) / (fromIntegral trials ** 3) / (10 ** 12)
 
   liftIO $ putStrLn $ printf "mean: %2.4f ms / var: %2.4f ms^2" mean var
 
@@ -50,26 +48,8 @@ bench str trials m = do
       let diff = Clock.toNanoSecs (Clock.diffTimeSpec e s)
       return (r, diff)
 
-
-
-
-
-
-newtype M a = M (ReaderT TypingContext IO a)
-  deriving (Functor, Applicative, Monad, MonadIO, MonadReader TypingContext, MonadCatch, MonadThrow)
-
-
-instance Has KeyTC TypingContext M where
-  ask _ = Control.Monad.Reader.ask
-
-instance Local KeyTC TypingContext M where
-  local _ = Control.Monad.Reader.local
-
-instance Has KeyDebugLevel Int M where
-  ask _ = return 0
-
-runM :: M a -> IO a
-runM (M m) = do
+runM :: TC a -> IO a
+runM (TC m) = do
   tc <- initTypingContext
   runReaderT m tc
 
@@ -80,17 +60,17 @@ checkType fp trials = runM $ do
 
   -- For simplificity, we ignore imports here
 
-  let nameTable = miNameTable baseModuleInfo
-  let opTable   = miOpTable   baseModuleInfo
+  let nameTable = mcNameTable (miModuleContext baseModuleInfo)
+  let opTable = mcOpTable (miModuleContext baseModuleInfo)
 
   let nameError (l, d) = staticError (nest 2 (ppr l </> d))
 
   (renamedDecls, tyDecls, synDecls, _newNames, _newOpTable) <-
     liftIO $ either nameError return $ runRenaming nameTable opTable $ renameTopDecls currentModule decls
 
-  let tyEnv = miTypeTable baseModuleInfo
-  let conEnv = miConTable baseModuleInfo
-  let synEnv = miSynTable baseModuleInfo
+  let tyEnv = mcTypeTable (miModuleContext baseModuleInfo)
+  let conEnv = mcConTable (miModuleContext baseModuleInfo)
+  let synEnv = mcSynTable (miModuleContext baseModuleInfo)
 
   let comp = do
         (_typedDecls, nts, _dataDecls', _typeDecls', _newCTypeTable, _newSynTable) <-
@@ -116,7 +96,7 @@ checkType fp trials = runM $ do
 
 main :: IO ()
 main = do
-  checkType "./Examples/F.sparcl"       1000
+  checkType "./Examples/F.sparcl" 1000
   checkType "./Examples/GV_func.sparcl" 1000
-  checkType "./Examples/App1.sparcl"    10000
-  checkType "./Examples/App10.sparcl"   10000
+  checkType "./Examples/App1.sparcl" 10000
+  checkType "./Examples/App10.sparcl" 10000
